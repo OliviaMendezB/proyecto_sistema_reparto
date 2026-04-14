1d3c4e8c33510e5daeb8fff09ca8d8d232d82f8a
helenaagc <helenaglez0@gmail.com>
2026-04-14 20:35:53 +0200

    Envio.hpp

---
+++ sistema_repartos/Envio.hpp
@@ -0,0 +1,32 @@
+#ifndef ENVIO_HPP
+#define ENVIO_HPP
+
+#include <string>
+
+class Envio {
+protected:
+    std::string id;
+    std::string cp;
+    bool cargado;
+    static int totalEnviosCreados;  // Variable estática
+    const int idNumerico;            // Variable constante
+
+public:
+    Envio(std::string id, std::string cp);
+    virtual ~Envio();
+
+    virtual void mostrar() const = 0;
+    virtual void mostrar(bool detallado) const = 0;  // Sobrecarga
+    virtual int prioridadCarga() const = 0;
+    virtual std::string tipo() const = 0;
+
+    std::string getId() const;
+    std::string getCP() const;
+    bool estaCargado() const;
+    void setCargado(bool c);
+
+    static int getTotalEnvios();      // Método estático
+    int getIdNumerico() const;        // Método const
+};
+
+#endif
