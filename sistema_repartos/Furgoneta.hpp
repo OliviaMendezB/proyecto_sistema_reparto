0c655969d58c8d7a8528a8602072d7c7d35e3a66
helenaagc <helena.gonzalez@cunef.edu>
2026-04-15 19:49:00 +0200

    Furgoneta.hpp

---
+++ sistema_repartos/Furgoneta.hpp
@@ -0,0 +1,16 @@
+#ifndef FURGONETA_HPP
+#define FURGONETA_HPP
+
+#include "Vehiculo.hpp"
+
+class Furgoneta : public Vehiculo {
+public:
+    Furgoneta(std::string id, std::string cp);
+    ~Furgoneta();
+
+    bool cargar(Envio* e) override;
+    void mostrar() const override;
+    void mostrar(bool mostrarCarga) const override;
+};
+
+#endif
